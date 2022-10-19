fb454e0c0e2aae78c46b54bc8dd93df4cafa9511
Florian <f.mewes87@gmail.com>
2022-10-19 11:15:21 +0200

    initial

--- shirt-shop/src/App.js
+++ shirt-shop/src/App.js
@@ -1,19 +1,19 @@
 // Packages Import
-import { FontAwesomeIcon } from "fortawesome/react-fontawesome";
-import { library } from 'fortawesome/fontawesome-svg-core';
+// import { FontAwesomeIcon } from "fortawesome/react-fontawesome";
+// import { library } from 'fortawesome/fontawesome-svg-core';
 import { Routes, Route, Link, Navigate } from 'react-router-dom';
 
 // Styles Import
-
-
-// Files Import
-
 import './styles/App.scss';
 
+// Files Import
+import data from "./data/products.json"
+
 function App() {
+  console.log(data);
   return (
     <div className="App">
-    
+        <p>hallo</p>
     </div>
   );
 }
