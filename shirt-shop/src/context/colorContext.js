36cda4cacee3b61ae08cca9e6110e4540471a8e2
magross <grossesbewirken@gmail.com>
2022-10-19 20:24:34 +0200

    farben in buttons. aber die default

--- shirt-shop/src/context/colorContext.js
+++ shirt-shop/src/context/colorContext.js
@@ -4,8 +4,24 @@ const ColorContext = createContext("", ()=>{})
 
 const ColorContextProvider = (props)=>{
   const colorContext = useState({
-    red:"rgb(200,0,50)",
-    
+    red:"rgb(200, 0, 50)",
+    darkred:"rgb(125, 0, 50)",
+    orange:"rgb(250, 125, 0)",
+    darkorange:"rgb(175, 75, 0)",
+    lime:"rgb(125, 250, 0)",
+    darklime:"rgb(50, 125, 0)",
+    grey:"rgb(125, 125, 125)",
+    darkgrey:"rgb(75, 75, 75)",
+    white:"rgb(250, 250, 250)",
+    darkwhite:"rgb(200, 200, 200)",
+    black:"rgb(25, 25, 25)",
+    darkblack:"rgb(0, 0, 0)",
+    oliv:"rgb(100, 100, 0)",
+    darkoliv:"rgb(60, 60, 60)",
+    beige:"rgb(250, 200, 125)",
+    darkbeige:"rgb(200, 150, 75)",
+    blue:"rgb(0, 100, 200)",
+    darkblue:"rgb(0, 50, 150)",
   })
   return <ColorContext.Provider value={colorContext}>{props.children}</ColorContext.Provider>
 }
