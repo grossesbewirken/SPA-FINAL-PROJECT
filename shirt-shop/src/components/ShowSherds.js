955b5c6d86d2698301241d012a4882fd9c678321
Florian <f.mewes87@gmail.com>
2022-10-22 11:55:52 +0200

    farbwechsel nach jedem rendern behoben favoriten button auf showSherds implementiert

--- shirt-shop/src/components/ShowSherds.js
+++ shirt-shop/src/components/ShowSherds.js
@@ -1,7 +1,16 @@
+import { useContext, useEffect, useState } from "react";
+
 import "../styles/showsherds.css"
+import FavoriteContext from "../context/FavoriteContext";
 
 import { Link } from "react-router-dom";
 const ShowSherds = ({sherd}) => {
+const [favorite, setFavorite] = useContext(FavoriteContext)
+const [randomColor, setRandomColor] = useState("black")
+
+const colorPalette = ["beige", "blue", "grey", "lime", "oliv", "orange","black", "red"]
+console.log("random Color:", randomColor);
+console.log(sherd);
 const textStyle = {
   position: "absolute",
   top:0,
@@ -15,13 +24,20 @@ const textStyle = {
   textAlign:"center",
 }
 
-const colorPalette = ["beige", "blue", "grey", "lime", "oliv", "orange","black", "red"]
+useEffect(()=>{
+  setRandomColor(colorPalette[Math.floor(Math.random() * colorPalette.length)])
+},[])
+
+const favoriteHandler = () => {
+  const newFav = [...favorite, {...sherd, backgroundColor: randomColor}]
+  setFavorite(newFav)
+}
 
   return (
     <div className="sherds-all black-grad-135">
       <Link className="link " to={`/products/${sherd.id}`}>
         <div className="img-container-all">
-          <img src={sherd.sherdColor[colorPalette[Math.floor(Math.random() * colorPalette.length)]]} alt="shirt" width="200" /> 
+          <img src={sherd.sherdColor[randomColor]} alt="shirt" width="200" /> 
           <div className="text-container-all" style={textStyle}>
             <p className="sherd-text-all">{sherd.text}</p> 
           </div>            
@@ -33,6 +49,10 @@ const colorPalette = ["beige", "blue", "grey", "lime", "oliv", "orange","black",
           <span className="text-details-all red-text">price: </span>
           <span>{sherd.price.toFixed(2)} €uro</span>
           <span className="red-text">{" }"}</span>
+        </div>
+        <div>
+          <button onClick={favoriteHandler}>FAV</button>
+          <button>BUY</button>
         </div>      
       </div>
   );
