823725eef921c29137d1b80393a0b49aea48ae2a
Florian <f.mewes87@gmail.com>
2022-10-24 11:29:03 +0200

    Anzhal an Warenkorb und Favoriten einblenden.

--- shirt-shop/src/components/Carousell.js
+++ shirt-shop/src/components/Carousell.js
@@ -1,9 +1,6 @@
 // Packages Import
 import Carousel from 'react-bootstrap/Carousel';
 import { Link } from 'react-router-dom';
-import { useContext} from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 // Styles Import
 import '../styles/carousel.scss';
@@ -12,26 +9,10 @@ import "../styles/App.scss";
 // Files Import
 import sherds from "../data/products";
 import fjm from '../images/fjm-logo.png';
-const Carousell = ({showCarousel, currColor, setCurrColor, counter}) => {
-  const [favorite, setFavorite] = useContext(FavoriteContext);
-  const [good, setGood] = useContext(ShoppingContext);
-
-
-  const favoriteHandler = (sherd) => {
-    const newFav = [...favorite, { ...sherd}];
-    setFavorite(newFav);
-  }
-    
-  const shoppingHandler = (sherd) => {
-    const newGood = [...good, { ...sherd}];
-    setGood(newGood);
-  }
+const Carousell = ({showCarousel, counter}) => {
 
   return (
     <div className='carousel-outer-container'>
-      {/* <h1 className="carousell-headline display-1" >
-        nerdsherd
-      </h1> */}
       {counter < 1
         ? 
       (<h1 className="carousell-headline display-1" >
