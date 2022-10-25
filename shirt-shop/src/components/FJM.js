3dfc317830772933de9dc46ba1be324aac2539b8
grossesbewirken <grossesbewirken@gmail.com>
2022-10-25 11:16:55 +0200

    fjm gestylt

--- shirt-shop/src/components/FJM.js
+++ shirt-shop/src/components/FJM.js
@@ -26,14 +26,13 @@ const FJM = () => {
           className="d-inline-block align-top"
           alt="FJM logo"
         />
-        <h5>The "nerdsherd" Shop was created as a final project for the "Single Page Application Module". 
-        </h5>
-        <h5>"nerdsherd" was created and designed by "FJM" which consists of Florian, Jeff and Martin. This three guys are amazing DCI Web Developer Students.</h5>
-        <h5>Have a look at our Profiles and at our other Projects on Github</h5>
+        <h6>"nerdsherd" was created and designed by "FJM" which consists of Florian, Jeff and Martin as the final project for the "Single Page Application Module".
+        </h6>
       </div>
       <div className="profile-card-container">
-      <div className='single-profile-card'>
-        
+
+
+      <div className='single-profile-card black-grad-135'>
         <img src={itzFlorian } alt="" />
         <h3>Florian Mewes</h3>
         <p>Chabos wissen wer der Babo ist...</p>
@@ -51,25 +50,9 @@ const FJM = () => {
             <p>itzFlorian</p>
           </div>
       </div>
-      <div className='single-profile-card'>
-        <img src={grossesbewirken} alt="" />
-        <h3>Martin Gross</h3>
-        <p>I am on going Web Developer</p>
-        <div className="link flexi">
-            <a
-              href="https://github.com/grossesbewirken"
-              target="_blank"
-              rel="noopener noreferrer">
-              <FontAwesomeIcon
-                icon={faGithub}
-                size='2x'
-                className='link'
-              />
-            </a>
-            <p>grossesbewirken</p>
-          </div>
-      </div>
-      <div className='single-profile-card'>
+
+
+      <div className='single-profile-card black-grad-135'>
         <img src={BreffJaun} alt="" />
         <h3>Jeff Braun</h3>
         <p>Hello, my name is Jeff and i'm web developer student, class representative and tutor at the DCI in germany.</p>
@@ -87,6 +70,35 @@ const FJM = () => {
             <p>BreffJaun</p>
           </div>
         </div>
+
+
+
+      <div className='single-profile-card black-grad-135'>
+        <img src={grossesbewirken} alt="" />
+        <h3>Martin Gross</h3>
+        <p>Hi, i´m a high skilled news media designer, experienced in print product development and print design. And now it´s time for the internet.</p>
+        <div className="link flexi">
+            <a
+              href="https://github.com/grossesbewirken"
+              target="_blank"
+              rel="noopener noreferrer">
+              <FontAwesomeIcon
+                icon={faGithub}
+                size='2x'
+                className='link'
+              />
+            </a>
+            <p>grossesbewirken</p>
+          </div>
+      </div>
+
+
+
+
+      </div>
+      <div className="general-info-container">
+      <h6> This three guys are amazing DCI Web Developer Students.</h6>
+      <h6>Have a look at our Profiles and at our other Projects on Github</h6>
       </div>
     </div>
   );
